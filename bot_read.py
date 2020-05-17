97ffed8809f6a20a734924881a21721a9a91a4a1
Devin Macalalad <devinmacalaladdt@gmail.com>
2020-05-17 12:04:37 -0400

    adds ticker to output

--- bot_read.py
+++ bot_read.py
@@ -17,6 +17,7 @@ for submission in subreddit.hot(limit=1):
         for comment in submission.comments:
             for t in tickers:
                 if re.search(t, comment.body, re.IGNORECASE):
+                    f.write(t+"\n")
                     f.write("["+(datetime.utcfromtimestamp(float(comment.created_utc))).strftime('%m/%d/%Y---%H:%M')+"]\n"+comment.body+"\n")
                     for reply in comment.replies:
                         f.write("\t->"+reply.body+"\n")
